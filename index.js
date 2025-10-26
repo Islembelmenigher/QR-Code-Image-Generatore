75bb75a9397fe29eb5504af819f8c22f3a38d62c
islem belmenigher <belmenigherislem@gmail.com>
2025-10-26 18:18:36 +0100

    full project

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+import { input } from '@inquirer/prompts';
+import fs from 'fs';
+import qr from 'qr-image'; 
+const answer = await input({ message: 'Enter your name' });
+
+
+
+fs.writeFile('URL.txt', answer, (err) => {
+  if (err) {
+    console.error('Error writing file:', err);
+  } else {
+    console.log('File created and text saved successfully!');
+  }
+});
+
+
+var qr_svg = qr.image(answer);
+qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+
+// commit to github
